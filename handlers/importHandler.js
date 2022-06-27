b7283a4692593ccf3067b56ae40b4a23d24830c5
Karel <33254498+endlessoblivion@users.noreply.github.com>
2022-06-27 10:02:46 +0200

    multi-pass of `import` handler, also multi-line

--- handlers/importHandler.js
+++ handlers/importHandler.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function importHandler(value, context, request, { isHeaders } = {}) {
-    if (!/^#import/m.test(value)) return value;
+    if (!/#import/m.test(value)) return value;
 
     return value
-      .replace(/^#import (.*);/m, function (includeStatement, file) {
-          const importThisFile = file.replace(/['"]/g, '');
+      .replace(/#import([^;]*);/mg, function (includeStatement, file) {
+          const importThisFile = file.trim().replace(/['"]/g, '');
           const content = fs.readFileSync(path.join(context, importThisFile));
           if (importThisFile.endsWith('.js')) {
               return (isHeaders ? _ => _ : JSON.stringify)(eval(content.toString()));
